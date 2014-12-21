509eafa4e6fdcad02d4b176cdbb41a8278ead554
pixelateddwarf <pixelateddwarf@gmail.com>
2014-12-21 13:24:11 -0500

    connect user to places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,7 @@
 class PlacesController < ApplicationController
 
+ before_action :authenticate_user!, :only => [:new, :create]
+
   def index
     @places = Place.paginate(:per_page => 2, :page => params[:page])
     #@places = Place.all # list all of the places in the Db
@@ -11,7 +13,7 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-root 'places#index'
+  root 'places#index'
 
   resources :places
 
