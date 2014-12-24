979bc2224289fb06da418adfd28f3b556f608fd9
pixelateddwarf <pixelateddwarf@gmail.com>
2014-12-24 12:09:57 -0500

    allow destroy for logged on users

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
 
- before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
+ before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.paginate(:per_page => 2, :page => params[:page])
@@ -43,6 +43,9 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+     if @place.user != current_user
+        return render :text => 'Not Allowed', :status => :forbidden
+     end
     @place.destroy
     redirect_to root_path
   end
