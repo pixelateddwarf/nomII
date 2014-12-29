c87c673d64e7b25a1d2ea7b4f89ca31ccd79d82f
vagrant <vagrant@precise32.(none)>
2014-12-29 16:53:23 +0000

    first commit from mac - added validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,9 +13,13 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
+  @place = current_user.places.create(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :new, :status => :unprocessable_entity 
   end
+end
 
   def show
     @place = Place.find(params[:id])
@@ -38,7 +42,11 @@ class PlacesController < ApplicationController
     end
 
     @place.update_attributes(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :edit, :status => :unprocessable_entity
+  end
   end
 
   def destroy
