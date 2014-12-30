56d2c00f0f7611b9a849c4f7f6bfe948fab43147
vagrant <vagrant@precise32.(none)>
2014-12-30 16:58:23 +0000

    “CAdded validations to place controller

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, :presence => true
+  validates :address, :presence => true, :length => { :maximum => 140, :minimum => 3 }
+  validates :description, :presence => true, :length => { :maximum => 200, :minimum => 3 }
+  validates :name, :presence => true, :length => { :maximum => 100, :minimum => 3 }
 end
