9b76fecb5903ce41658b61ea58aa74fc1427871e
vagrant <vagrant@precise32.(none)>
2014-12-31 20:12:46 +0000

    geocode added

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,11 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  
+  geocoded_by :address
+  after_validation :geocode
+
+  validates :name, :presence => true, :length => { :maximum => 100, :minimum => 3 }
   validates :address, :presence => true, :length => { :maximum => 140, :minimum => 3 }
   validates :description, :presence => true, :length => { :maximum => 200, :minimum => 3 }
-  validates :name, :presence => true, :length => { :maximum => 100, :minimum => 3 }
+
 end
